1926df7fcba0347a9ea91aa1329b3a6d7de33aad
Mordnor <dorian.pitiot@gmail.com>
2018-06-28 16:16:26 +0200

    list done

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,21 @@
 // Drag and Drop Function
-$(document).ready(function(){
+$(document).ready(function() {
 
- function supp() {
-   $(".remove").click(function(event) {
-     $(this).parents('li').remove();
-   });
-};
+    function supp() {
+        $(".remove").click(function(event) {
+            $(this).parents('li').remove();
+        });
+    };
 
 
-supp()
+    supp()
 
-$("#add").click(function(event) {
-var task = $(".addText").val()
-console.log(task);
-$("<li class='toDo'>" + task + "<i class='remove'> ✖ </i>" +  "</li>").appendTo('#items')
-supp()
-});
+    $("#add").click(function(event) {
+        var task = $(".addText").val()
+        console.log(task);
+        $("<li class='toDo'>" + task + "<i class='remove'> ✖ </i>" + "</li>").appendTo('#items')
+        supp()
+    });
 });
 
 var el = document.getElementById('items');
@@ -23,27 +23,36 @@ var sortable = Sortable.create(el);
 
 
 
-$(document).ready(function(){
+$(document).ready(function() {
     // Timer Countdown
-    var clock = $('.clock').FlipClock(1499, {
+    var clock = $('.clock').FlipClock(4, {
         autoStart: false,
-        clockFace: 'MinuteCounter'
+        clockFace: 'MinuteCounter',
+        onInterval: function() {
+            if (this.getFaceValue() == 0) {
+                console.log($(".toDo")[0]);
+                $("#done").append($(".toDo")[0]);
+            }
+        }
+
     });
+    var time = clock.getFaceValue();
+    console.log(time);
     clock.setCountdown(true);
     clock.stop();
     // Event button
     var btn_start = $('#btn_start');
-    btn_start.click(function(){
+    btn_start.click(function() {
         clock.start();
     })
 
     var btn_pause = $('#btn_pause');
-    btn_pause.click(function(){
+    btn_pause.click(function() {
         clock.stop();
     })
 
     var btn_reset = $('#btn_reset');
-    btn_reset.click(function(){
+    btn_reset.click(function() {
         clock.reset();
     })
 

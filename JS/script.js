d7e895bd5c1d40f7732be869efbe213bcf06bfc4
Mordnor <dorian.pitiot@gmail.com>
2018-06-28 13:43:54 +0200

    button flipclock

--- JS/script.js
+++ JS/script.js
@@ -2,10 +2,29 @@
 var el = document.getElementById('items');
 var sortable = Sortable.create(el);
 
+
+
 $(document).ready(function(){
-    var clock = $('.clock').FlipClock(1500, {
-        countdown: true,
+    // Timer Countdown
+    var clock = $('.clock').FlipClock(1499, {
+        autoStart: false,
         clockFace: 'MinuteCounter'
     });
+    clock.setCountdown(true);
+    clock.stop();
+    // Event button
+    var btn_start = $('#btn_start');
+    btn_start.click(function(){
+        clock.start();
+    })
 
+    var btn_pause = $('#btn_pause');
+    btn_pause.click(function(){
+        clock.stop();
+    })
+
+    var btn_reset = $('#btn_reset');
+    btn_reset.click(function(){
+        clock.reset();
+    })
 });
